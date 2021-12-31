63d13552e362dc59b89f4d4e1a71727a3d03b30d
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-12-31 15:46:40 +0545

    add flight function

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:flight_id>", views.flight, name="flight")
 ]
